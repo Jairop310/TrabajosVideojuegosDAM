cb03373a1de5c87f4166103d2f8efcb3d96736b0
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2024-11-27 14:30:24 +0100

    Cosas

--- DAMVideo/Assets/Scripts/Saludos.cs
+++ DAMVideo/Assets/Scripts/Saludos.cs
@@ -7,16 +7,26 @@ public class Saludos : MonoBehaviour
 {
     public string presentacion = "Hello World";
     private int ContadorFrame = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log(presentacion);
+        print(transform.position);
+        print(transform.rotation);
+        print(transform.localScale);
+        print(transform.eulerAngles);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        ContadorFrame++;
-        Debug.Log(ContadorFrame);
+        // ContadorFrame++;
+        // Debug.Log(ContadorFrame);
+        /*print(transform.position);
+        print(transform.rotation);
+        print(transform.localScale);
+        print(transform.eulerAngles);*/
     }
 }
---
+++ DAMVideo/Assets/Scripts/newPosition.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newPosition : MonoBehaviour
+{
+    [Header("Posicion inical")]
+    public Vector3 posicionInicial;
+
+    [Header("Posiciones")]
+    [SerializeField] private float movimientoX = 0f;  
+    [SerializeField] private float movimientoZ = 0f;  
+
+    [Header("Velocidades2")]
+    public float velocidadMovimiento = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Puedes inicializar la posición aquí si lo deseas
+        posicionInicial = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimiento en el eje X
+        if (Input.GetKey(KeyCode.A)) movimientoX -= velocidadMovimiento * Time.deltaTime;  // Mover a la izquierda
+        if (Input.GetKey(KeyCode.D)) movimientoX += velocidadMovimiento * Time.deltaTime;  // Mover a la derecha
+
+        // Movimiento en el eje Z
+        if (Input.GetKey(KeyCode.S)) movimientoZ -= velocidadMovimiento * Time.deltaTime;  // Mover hacia atrás
+        if (Input.GetKey(KeyCode.W)) movimientoZ += velocidadMovimiento * Time.deltaTime;  // Mover hacia adelante
+
+        // Actualizar la posición del objeto sumando el movimiento relativo a la posición inicial
+        transform.position = new Vector3(posicionInicial.x + movimientoX, 0.5f, posicionInicial.z + movimientoZ);
+    }
+}
