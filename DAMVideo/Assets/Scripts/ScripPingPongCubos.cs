d9e1cd55aa51fd13b0f6db0283a62d54d2e06b53
Jairo Piña Salas <jairo.ps.gdm@gmail.com>
2024-11-29 14:04:06 +0100

    AAAAAAA

--- DAMVideo/Assets/Scripts/ScripPingPongCubos.cs
+++ DAMVideo/Assets/Scripts/ScripPingPongCubos.cs
@@ -1,37 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ScripPingPongCubos : MonoBehaviour
 {
-    public MeshRenderer[] meshRenderer;
-    public Color[] color;
+    public GameObject[] cajas;
 
+    public Color[] color;
     public Vector3[] direcciones;
-    public Transform[] bloques;
+
     
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < meshRenderer.Length; i++)
+        for (int i = 0; i < cajas.Length; i++)
         {
-            meshRenderer[i].material.color = color[i];
+            cajas[i].GetComponent<MeshRenderer>().material.color = color[i];
         }
-        
         direcciones = new Vector3[] {Vector3.right,Vector3.left,Vector3.right,Vector3.left};
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < bloques.Length; i++)
+        for (int i = 0; i < cajas.Length; i++)
         {
-            bloques[i].position += (i + 1) * direcciones[i] * Time.deltaTime;
-            if (bloques[i].position.x < -4)
-            {
+            cajas[i].transform.position += (i + 1) * direcciones[i] * Time.deltaTime;
+            if (cajas[i].transform.position.x < -4) { 
+
                 direcciones[i] = Vector3.right;
             }
-            else if (bloques[i].position.x > 4)
+            else if (cajas[i].transform.position.x > 4)
             {
                 direcciones[i] = Vector3.left;
             }
